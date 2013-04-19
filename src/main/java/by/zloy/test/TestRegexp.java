749d5c1b4a632910d818230525741b8e378f71be
Eugene Kozlov <evgeny.kozlov@oxagile.com>
2013-04-19 13:20:03 +0300

    Refactoring

---
+++ src/main/java/by/zloy/test/TestRegexp.java
@@ -0,0 +1,24 @@
+package by.zloy.test;
+
+import java.util.regex.Pattern;
+
+/**
+ * User: eugene
+ * Date: 4/19/13
+ * Time: 1:09 PM
+ */
+public class TestRegexp {
+	public final static Pattern NUMBER_PATTERN = Pattern.compile("[-+]?[0-9]*[\\,\\.]?[0-9]+");
+
+	public static void main(String[] args) {
+		System.out.println(NUMBER_PATTERN.matcher("1").matches());
+		System.out.println(NUMBER_PATTERN.matcher("-1").matches());
+		System.out.println(NUMBER_PATTERN.matcher("2.").matches());
+		System.out.println(NUMBER_PATTERN.matcher("-2.").matches());
+		System.out.println(NUMBER_PATTERN.matcher("2,").matches());
+		System.out.println(NUMBER_PATTERN.matcher("-2,").matches());
+		System.out.println(NUMBER_PATTERN.matcher("3,1415").matches());
+		System.out.println(NUMBER_PATTERN.matcher("-3,1415").matches());
+
+	}
+}

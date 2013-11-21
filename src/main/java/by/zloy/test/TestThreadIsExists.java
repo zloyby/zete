589d281d8d0f448ae11752c113befcf889eca472
Eugene Kozlov <evgeny.kozlov@oxagile.com>
2013-11-21 12:50:09 +0300

    GetHttpTest

--- src/main/java/by/zloy/test/TestThreadIsExists.java
+++ src/main/java/by/zloy/test/TestThreadIsExists.java
@@ -1,26 +1,26 @@
 package by.zloy.test;
 
 public class TestThreadIsExists {
-    public static void main(String[] args) {
-        TestThreadIsExistsToRunInBackground t = new TestThreadIsExistsToRunInBackground();
-        Thread thread = new Thread(t);
-        System.out.println("Prepare thread");
-        thread.start();
-        System.out.println("Final string");
-        //System.exit(0); // thread is fucked!
-    }
+	public static void main(String[] args) {
+		TestThreadIsExistsToRunInBackground t = new TestThreadIsExistsToRunInBackground();
+		Thread thread = new Thread(t);
+		System.out.println("Prepare thread");
+		thread.start();
+		System.out.println("Final string");
+		//System.exit(0); // thread is fucked!
+	}
 
-    static class TestThreadIsExistsToRunInBackground implements Runnable {
-        @Override
-        public void run() {
-            try {
-                System.out.println("Thread begins working");
-                Thread.sleep(5000);
-                System.out.println("Thread ends working");
-            } catch (InterruptedException e) {
-                e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
-            }
-        }
-    }
+	static class TestThreadIsExistsToRunInBackground implements Runnable {
+		@Override
+		public void run() {
+			try {
+				System.out.println("Thread begins working");
+				Thread.sleep(5000);
+				System.out.println("Thread ends working");
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+	}
 
 }

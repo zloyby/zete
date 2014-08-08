7f2de6462f350cb93ca940bd936dd21a4c3260b7
Eugene Kozlov <evgeny.kozlov@oxagile.com>
2014-08-08 11:52:30 +0300

    tests

--- src/main/java/by/zloy/interview/ExtendsWithConstructor.java
+++ src/main/java/by/zloy/interview/ExtendsWithConstructor.java
@@ -6,25 +6,57 @@ public class ExtendsWithConstructor {
         test.f();
     }
 
+    @SuppressWarnings("UnusedDeclaration")
     public static class A {
+        Long defaultNumber;
+
         public A() {
             System.out.println("Constructor A");
-            f();
+            defaultNumber = 1L;
+            f(defaultNumber);
         }
 
-        public void f() {
-            System.out.println("Parent");
+        protected void f() {
+            System.out.println("Parent None " + defaultNumber);
+        }
+
+        void f(Number number) {
+            System.out.println("Parent Number " + defaultNumber + " " + number);
+        }
+
+        private void f(Integer number) {
+            System.out.println("Parent Integer " + defaultNumber + " " + number);
+        }
+
+        public void f(Long number) {
+            System.out.println("Parent Long " + defaultNumber + " " + number);
         }
     }
 
+    @SuppressWarnings("UnusedDeclaration")
     public static class B extends A {
+        Integer defaultNumber;
+
         public B() {
             System.out.println("Constructor B");
-            f();
+            defaultNumber = 2;
+            f(defaultNumber);
         }
 
         public void f() {
-            System.out.println("Child");
+            System.out.println("Child None " + defaultNumber);
+        }
+
+        public void f(Number number) {
+            System.out.println("Child Number " + defaultNumber + " " + number);
+        }
+
+        public void f(Integer number) {
+            System.out.println("Child Integer " + defaultNumber + " " + number);
+        }
+
+        public void f(Long number) {
+            System.out.println("Child Long " + defaultNumber + " " + number);
         }
     }
 }

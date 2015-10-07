bf727e0c33347fc70768e3b270d6aaf1a516ee72
evgeny.kozlov@oxagile.com <evgeny.kozlov@oxagile.com>
2015-10-07 13:38:14 +0300

    Test enum

--- src/main/java/by/zloy/test/TestEnum.java
+++ src/main/java/by/zloy/test/TestEnum.java
@@ -1,10 +1,31 @@
 package by.zloy.test;
 
-public enum TestEnum {
+// enum showing Mobile prices
+enum Mobile {
+    Samsung(400), Nokia(250), Motorola(325);
 
-    BEST_SINGLETON;
+    int price;
 
-    public static TestEnum getInstance() {
-        return BEST_SINGLETON;
+    Mobile(int p) {
+        price = p;
+    }
+
+    int showPrice() {
+        return price;
+    }
+}
+
+public class TestEnum {
+
+    public static void main(String args[]) {
+
+        System.out.println("CellPhone List:");
+        for (Mobile m : Mobile.values()) {
+            System.out.println(m + " costs " + m.showPrice() + " dollars");
+        }
+
+        Mobile ret;
+        ret = Mobile.valueOf("Samsung");
+        System.out.println("Selected : " + ret);
     }
 }

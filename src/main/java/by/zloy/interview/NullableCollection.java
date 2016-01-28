7fa6aa0f3f4f5e6760a574f6c126415e61f5bf0e
evgeny.kozlov <e.kozlov@hotmail.com>
2016-01-28 12:10:26 +0300

    Null fix

--- src/main/java/by/zloy/interview/NullableCollection.java
+++ src/main/java/by/zloy/interview/NullableCollection.java
@@ -1,8 +1,8 @@
 package by.zloy.interview;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
@@ -12,22 +12,25 @@ public class NullableCollection {
 
     static Map<String, Integer> map = new HashMap();
     static Set<Integer> set = new HashSet();
-    static List<String> list = new ArrayList();
+    static List<String> list = new LinkedList<String>();
 
     public static void main(String[] args) {
         NullableCollection.run();
     }
 
     private static void run() {
-        set.add(null);
-        set.add(null);
         list.add(null);
         list.add(null);
+        System.out.println(list.size());
+
+        set.add(null);
+        set.add(null);
+        System.out.println(set.size());
+
         Object a1 = map.put(null, 1);
         Object a2 = map.put(null, 2);
-
         System.out.println(map.size());
-        System.out.println(list.size());
-        System.out.println(set.size());
+        System.out.println(a1);
+        System.out.println(a2);
     }
 }
